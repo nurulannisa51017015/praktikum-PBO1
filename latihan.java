03367f3bb6ffafed8ceb7db58cb3a3c725b6dd48
nurulannisa51017015 <48232551+nurulannisa51017015@users.noreply.github.com>
2019-09-11 21:27:45 -0700

    praktikum PBO1
    praktikum PBO1

---
+++ latihan.java
@@ -0,0 +1,5 @@
+public class latihan{
+	public static void main(String args[]) {
+		long p=214748648;
+	}
+}
---
+++ percobaan6.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class percobaan6 {
+	public static void main (String [] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int nilai1, nilai2, hasil;
+		
+		System.out.print("Masukan integer pertama : ");
+		nilai1 = input.nextInt ();
+		
+		System.out.print("Masukan integer kedua :");
+		nilai2 = input.nextInt ();
+		
+		hasil = nilai1 + nilai2;
+		System.out.println("Hasil " +nilai1+ " + " +nilai2+ " = " +hasil);
+		
+	}
+}
